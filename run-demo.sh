f5978822874d306da31c165a9a55d487e6cd9570
ellie <ellie@pacifist.studio>
2022-10-02 14:54:18 -0700

    Added a bash script to launch these on ubuntu

---
+++ run-demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# changes directory to the directory holding this script
+cd ${0%/*}
+
+python3 src/startserver.py &
+python3 src/startclient.py -player "Java" &
+python3 src/startclient.py -player "Scout" &
+python3 src/startclient.py -player "River" &
